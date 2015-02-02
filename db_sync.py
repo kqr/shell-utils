2d6238e7ab0997d449469ef373becc28cfedc9a7
chris <christoffer@agoodid.se>
2015-02-02 14:16:02 +0100

    made db_sync actually sync to local

--- db_sync.py
+++ db_sync.py
@@ -1,22 +1,52 @@
-import os, sys
+import os, re, sys
 from   functools    import partial
-from   subprocess   import Popen
+from   itertools    import chain
+from   subprocess   import Popen, PIPE
 from   urllib.parse import urlparse
 
 stderr = partial(print, file=sys.stderr)
 
 
+def argsify(d):
+  return ('--{}={}'.format(k, v) for k, v in d.items())
+
+
 def download_db(server):
-    args = ['--{}={}'.format(k, v) for k, v in {
+    dbname = server.path.lstrip('/')
+
+    create = Popen(['createdb', dbname], stderr=PIPE)
+    res = create.stderr.read().decode('utf-8')
+    create.wait()
+    if re.search(r'ERROR:  database "{}" already exists'.format(dbname), res):
+        print('Database "{}" already exists and will be '
+              'replaced. Continue? (y/N)'.format(dbname))
+        confirmation = input()
+        if confirmation not in ['y', 'Y']:
+            return None
+
+    os.environ['PGPASSWORD'] = server.password
+    remote_args = argsify({
         'host': server.hostname,
         'port': server.port,
         'username': server.username,
+        'dbname': dbname
+    })
+    dump = Popen(
+        chain(['pg_dump', '-Fc', '--no-acl', '--no-owner'], remote_args),
+        stdout=PIPE, stderr=sys.stderr
+    )
+
+    local_args = argsify({
+        'host': 'localhost',
+        'username': os.environ['USER'],
         'dbname': server.path.lstrip('/')
-    }.items()]
-    os.environ['PGPASSWORD'] = server.password
-    Popen(['pg_dump', '-Fc', '--no-acl', '--no-owner'] + args,
-          stdout=sys.stdout,
-          stderr=sys.stderr).communicate()
+    })
+    restore = Popen(
+        chain(['pg_restore', '--verbose', '--clean', '--no-acl', '--no-owner'], local_args),
+        stdin=dump.stdout, stdout=sys.stdout, stderr=sys.stderr
+    )
+
+    restore.wait()
 
 
 if __name__ == '__main__':
